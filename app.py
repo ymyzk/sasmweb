4c4c645d6d6759a644e19460066d3c43d5462908
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-05-23 05:20:54 +0900

    Fix

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import sys
 
-try:
-    import gevent.monkey
-    gevent.monkey.patch_all()
-except:
-    pass
 from bottle import route, run
 
 @route('/')
 def index(name):
-    return '<b>Hello</b>!'
+    return 'Hello!'
 
-run(server='gevent', host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+run(host='0.0.0.0', port=sys.argv[1])
