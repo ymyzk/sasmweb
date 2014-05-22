09c8ff285646620313ebcde23c7233164beae97e
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-05-23 05:15:37 +0900

    Bugfix

--- app.py
+++ app.py
@@ -3,10 +3,15 @@
 
 import os
 
+try:
+    import gevent.monkey
+    gevent.monkey.patch_all()
+except:
+    pass
 from bottle import route, run
 
 @route('/')
 def index(name):
     return '<b>Hello</b>!'
 
-run(server='gevent', port=int(os.environ.get("PORT", 5000)))
+run(server='gevent', host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
